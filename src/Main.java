b4929b898010d9c9704e22e8b8d533bfbba02918
Sarah Lagares <smlagares99@gmail.com>
2024-04-06 18:40:48 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //syntax
+
+        int i = 1;
+        while (i < 11) {
+            System.out.println(i++);
+        }
+
+    }
+}
